a9d27b6ec5aba30658f5cc8eaa940eab1bd1c877
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2022-08-07 17:07:23 +0530

    Time: 477 ms (27.71%), Space: 215.8 MB (5.09%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     bool isSubset(vector<int> &arr, int n, int sum, int **dp){
-        //memoization
+        //top-down approach
+        //iterative
+         
         
-        
-        if(sum==0) return 1;
-        if(n==0) return 0;
-        if(dp[n][sum] != -1) return dp[n][sum];
-        
-        if(arr[n-1] <= sum){
-            return dp[n][sum] = (isSubset(arr, n-1, sum-arr[n-1], dp) || isSubset(arr,n-1,sum, dp)); 
-        }else{
-            return dp[n][sum] = isSubset(arr,n-1,sum, dp);
+        //intitialization 
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0) dp[i][j] = 0;
+                if(j==0) dp[i][j] = 1;
+            }
         }
+        
+        //choice and optimize
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j]); 
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
     }
     bool canPartition(vector<int>& arr) {
         
