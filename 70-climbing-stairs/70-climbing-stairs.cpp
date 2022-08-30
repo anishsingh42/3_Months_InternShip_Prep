31341b2f8e9ef76a6d476dc02e95efad39976334
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2022-08-30 23:39:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (16.01%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int helper(int n, vector<int> &dp){
-        //memoization
+        //tabulation
         if(n==0)
             return 1;
-        if(n==1)
+        else if(n==1)
             return 1;
-        if(dp[n] != -1)
-            return dp[n];
-        
-        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
+        else{
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i=2;i<=n;i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
