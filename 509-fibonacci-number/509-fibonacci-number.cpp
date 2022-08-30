559ca164fb52184262dfe2d409b32ed1a1ce03a2
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2022-08-30 21:06:32 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int helper(int n, vector<int> &dp){
-        //memoization
-        if(n<=1){
-            return n;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
+        //tabulation 
+//         if(n<=1){
+//             return n;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
         
-        return dp[n] = helper(n-1, dp)+helper(n-2, dp);
+//         return dp[n] = helper(n-1, dp)+helper(n-2, dp);
+            
+            if(n==0){
+                return 0;
+            }
+            else if(n==1){
+                return 1;
+            }
+            else{
+                dp[0] = 0;
+                dp[1] = 1;
+
+                for(int i=2;i<=n;i++){
+                    dp[i] = dp[i-1]+dp[i-2];
+                }
+            }
+            return dp[n];
     }
     int fib(int n) {
         vector<int> dp(n+1 , -1);
