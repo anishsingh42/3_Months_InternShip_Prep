1174f5f06a13525d4a1ed2d9d39297f010792901
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2022-10-17 20:21:30 +0530

    Time: 9 ms (54.05%), Space: 9.7 MB (71.80%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,13 +4,11 @@ public:
         int h = nums.size()-1;
         int l = 0;
         int m;
-        int ind = -1;
         while(l <= h){
             m = l + (h-l)/2;
             if(nums[m] == target)
                 return m;
             else if(nums[m] < target){
-                ind = m;
                 l = m+1;
             }
             else{
