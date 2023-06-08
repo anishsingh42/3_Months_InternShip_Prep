e0f0acabb277fa5f03546e1862287ccd90491476
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-06-08 18:58:06 +0530

    Time: 12 ms (7.18%), Space: 11.2 MB (22.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0;
-        int h=nums.size()-1;
-        int m;
-        while(l<=h){
-            m = l+(h-l)/2;
-            if(nums[m] == target){
-                return m;
-            }
-            //rotation exists ; and mid in left monotonous part
-            else if(nums[m] > nums[h]){
-                if(nums[l] <= target && target <= nums[m])
-                    h=m-1;
-                else
-                    l=m+1;
-            }
-            //rotation exists ; and mid in the right monotonous part
-            else if(nums[m] < nums[l]){
-                if(nums[m] <= target && target <= nums[h])
-                    l=m+1;
-                else
-                    h=m-1;
-            }
-            //rotation does not exists 
-            else{
-                if(nums[m] < target)
-                    l = m+1;
-                else
-                    h=m-1;
-            }
+        //linear serach 
+        //brute
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target)
+                return i;
         }
         return -1;
     }
