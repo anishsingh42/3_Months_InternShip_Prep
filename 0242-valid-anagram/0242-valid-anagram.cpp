b07d0ea8038bdc1cea9cc7409ca46865fdd42b7f
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-06-19 12:15:29 +0530

    Time: 4 ms (95.63%), Space: 7.4 MB (42.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int mp[26] = {0};
+        
         if(s.size() != t.size()) return false;
+        
+        int alpha[26] = {0};
+        
         for(int i=0;i<s.size();i++){
-            mp[s[i] - 'a']++;
-            mp[t[i] - 'a']--;
+            alpha[s[i]-'a']++;
+            alpha[t[i]-'a']--;
         }
         
-        for(auto i:mp){
-            if(i) return false;
+        for(int i=0;i<26;i++){
+            if(alpha[i]  != 0) return false;
         }
+        
         return true;
     }
 };
