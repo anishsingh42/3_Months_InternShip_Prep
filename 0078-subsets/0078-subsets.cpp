4f58d20005b5ab60814a47fdf47fe6f03f1ed279
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-07-13 16:21:18 +0530

    Time: 312 ms (5.21%), Space: 105.3 MB (11.44%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> powerset;
-        int n = nums.size();
-        for(int num = 0; num < (1<<n); num++){
-            vector<int>subset;
-            for(int mask = 0; mask < n; mask++){
-                if(num & (1 << mask)) subset.push_back(nums[mask]);
-            }powerset.push_back(subset);
+    vector<vector<int>> solve(vector<int> & arr,int i, vector<int> op, vector<vector<int>>&soln){
+        if(i==arr.size()){
+            soln.push_back(op);
+            return soln;
         }
-        return powerset;
+        vector<int> op1 = op;
+        vector<int> op2 = op;
+        
+        op2.push_back(arr[i++]);
+        solve(arr,i,op1,soln);
+        solve(arr,i,op2,soln);
+        return soln;
+    }
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int>> soln;
+        vector<int>op;
+        solve(arr,0,op,soln);
+        return soln;
     }
 };
