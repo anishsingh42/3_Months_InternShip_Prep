8fb12edf0ea8c4d259a4ba1d03d6f14d2fe537a7
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-13 12:38:44 +0530

    Time: 3 ms (66.92%), Space: 14.9 MB (47.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void solve(int open, int close, string op, vector<string>& res){
-        //base condition
-        if(open == 0 and close == 0){
+    void solve(int open, int close, string op, vector<string> &res){
+        if(open == 0 and close == 0)
+        {
             res.push_back(op);
             return;
         }
         
-        //add open bracket
-        if(open!=0) solve(open-1, close, op+"(", res);        
-        //add close bracket
+        //add open brackets
+        if(open != 0) solve(open-1, close, op+"(", res);
+        //add the close brackets
         if(close > open) solve(open, close-1, op+")", res);
         return;
     }
+    
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         solve(n, n, "", res);
         return res;
     }
-    
 };
