722b1e8ab105b14887e96f1aa42aadb9b585163a
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-14 12:23:42 +0530

    Time: 11 ms (93.83%), Space: 18.2 MB (66.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -5,14 +5,14 @@ public:
         int col[9][9] = {0};
         int grid[3][3][9] = {0};
         
-        for(int r=0;r<9;r++){
-            for(int c=0;c<9;c++){
-                if(board[r][c] == '.') continue;
-                
-                int cnt = board[r][c] - '1';
-                if(row[r][cnt]++) return false;
-                if(col[cnt][c]++) return false;
-                if(grid[r/3][c/3][cnt]++) return false;
+        for(int r = 0; r < board.size(); r++){
+            for(int c = 0; c < board[0].size(); c++){
+                if(board[r][c] != '.'){
+                    int index = board[r][c] - '1';
+                    if(row[r][index]++) return false;
+                    if(col[index][c]++) return false;
+                    if(grid[r/3][c/3][index]++) return false;
+                }
             }
         }
         return true;
