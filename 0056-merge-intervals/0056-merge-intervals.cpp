9d2fec4eb4eba37469a887ec6be9b067c1e65c06
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-15 17:35:52 +0530

    Time: 26 ms (85.28%), Space: 19.3 MB (28.94%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,8 +5,8 @@ public:
         vector<vector<int>> res;
         int n = intervals.size();
         
-        for(int i=0;i<n;i++){
-            if(res.empty() or intervals[i][0] > res.back()[1]) res.push_back(intervals[i]);
+        for(int i = 0; i < n; i++){
+            if(res.empty() or res.back()[1] < intervals[i][0]) res.push_back(intervals[i]);
             else res.back()[1] = max(res.back()[1], intervals[i][1]);
         }
         
