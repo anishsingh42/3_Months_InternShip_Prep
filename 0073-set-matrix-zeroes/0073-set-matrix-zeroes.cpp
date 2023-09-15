8cf1ac41a689b4e4df2507cb22f6c6f6f14fecf4
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-15 20:10:35 +0530

    Time: 8 ms (89.14%), Space: 14 MB (6.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        vector<int> row(matrix.size(), 0);
+        vector<int> col(matrix[0].size(), 0);
         
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
-        
-        for(int i=0; i < n ;i++){
-            for(int j=0; j < m ;j++){
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
                 if(matrix[i][j] == 0){
                     row[i] = 1;
                     col[j] = 1;
@@ -17,44 +14,14 @@ public:
         }
         
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 or col[j]==1) 
-                    matrix[i][j]=0;
-            }
-        }
-        return;
-    }
-};
-
-/* 
-
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == 0) {  // Check if the current element is zero
-                    row[i] = 1;
-                    col[j] = 1;
-                }
-            }
-        }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(row[i] == 1 || col[j] == 1) {  // If either the row or column should be zero
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(row[i] == 1 or col[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
         }
+        
+        return;
     }
 };
-
-*/
