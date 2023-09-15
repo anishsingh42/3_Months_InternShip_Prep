a14f18b1a50a9fec572c0d6790c22a900c618d95
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-15 13:38:26 +0530

    Time: 86 ms (50.26%), Space: 68.1 MB (6.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr_sum = 0;
+        // moore vooting algorithm
+        int cnt_sum = 0;
         int max_sum = INT_MIN;
-        
-        for(int i=0;i<nums.size();i++){
-            curr_sum = max(nums[i], curr_sum+nums[i]);
-            max_sum = max(curr_sum, max_sum);
+        for(int i = 0; i < nums.size(); i++){
+            cnt_sum = max(nums[i], cnt_sum+nums[i]);
+            max_sum = max(cnt_sum, max_sum);
         }
         
         return max_sum;
