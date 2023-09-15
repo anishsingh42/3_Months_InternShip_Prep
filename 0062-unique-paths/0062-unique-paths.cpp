397120de0c35eedacdb4f3d14d8194510f5b6e6f
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-15 20:03:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    
-    
     int uniquePaths(int m, int n) {
+        //mathematics 
+        // permuation formula
         int right = n-1;
         int down = m-1;
-        
         long long ans = 1;
-        
-        for(int i=right+down, j=1; i> max(right, down); i--,j++){
-            // cout << i << " " << j << endl;
-            ans = (ans * i)/j;
+        for(int i = right + down, j = 1; j <= min(right, down); i--, j++){
+            ans = (ans*i) / j;
         }
         
-        return ans;
+        return (int) ans;
+        
     }
 };
