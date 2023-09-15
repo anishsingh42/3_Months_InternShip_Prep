69878a35655c843717729035fc2ffadce115dfb4
Anish Singh <71481218+anishsingh42@users.noreply.github.com>
2023-09-15 20:14:28 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (13.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //simple concet 
+        /*
+        * 3 colors and 3 different variables 
+        * low dnotes the number 0
+        * mid denotes the number 1 
+        * high denotes the number 2
+        **/
+        
         int low = 0;
         int mid = 0;
         int high = nums.size()-1;
         
-        
         while(mid <= high){
             switch(nums[mid]){
-                case 0: swap(nums[low], nums[mid]);
-                    low++;
-                    mid++;
-                    break;
-                case 1: mid++;
-                    break;
-                case 2: swap(nums[mid], nums[high]);
-                    high--;
-                    break;
+                case 0 : swap(nums[low], nums[mid]); low++; mid++; break;
+                case 1 : mid++; break;
+                case 2 : swap(nums[high], nums[mid]); high--; break;
+                
             }
         }
         
